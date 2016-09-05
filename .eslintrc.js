e259bb4d02840461e1fdfe432101bed1abcc88ff
Katryn McIntosh <katrynmcintosh@swipely.com>
2016-09-05 00:27:35 -0400

    Update eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  "extends": "eslint:recommended",
+ "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+          "jsx": true
+      }
+  },
+  "plugins": [
+    "react"
+  ],
   "rules": {
-    // enable additional rules
-    "indent": ["error", 4],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-
-    // override default options for rules from base configurations
-    "comma-dangle": ["error", "always"],
-    "no-cond-assign": ["error", "always"],
-
-    // disable rules from base configurations
-    "no-console": "off",
+    "quotes": [2, "single"],
   }
 }
