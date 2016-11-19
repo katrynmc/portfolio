d1aa136cc3cb8eae6faf8ef65fe62adc32c8025c
Katryn McIntosh <katrynmcintosh@swipely.com>
2016-11-19 18:35:18 -0500

    hopefully fix heroku

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 const app = Server.app()
 
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-
-
-
 console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.config.js')
   const compiler = webpack(config)
 
@@ -31,28 +28,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   app.use(webpackHotMiddleware(compiler))
 }
-else {
-  const config = require('./webpack.production.config.js')
-  const compiler = webpack(config)
 
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: false,
-    quiet: false,
-    lazy: false,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: true
-    },
-    stats: {
-        colors: true
-    },
-    reporter: null,
-    publicPath: config.output.publicPath
-  }))
-
-  app.use(webpackHotMiddleware(compiler))
-
-}
 
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
