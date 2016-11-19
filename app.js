2e8b9e874eef1a0dff058e92725c7c41f4f3dca6
Katryn McIntosh <katrynmcintosh@swipely.com>
2016-11-19 18:16:36 -0500

    Update middleware config

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ const Server = require('./server.js')
 const port = (process.env.PORT || 8080)
 const app = Server.app()
 
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+
+
+
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.config.js')
   const compiler = webpack(config)
 
-  app.use(webpackHotMiddleware(compiler))
   app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
+    noInfo: false,
+    quiet: false,
+    lazy: false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
+    },
+    stats: {
+        colors: true
+    },
+    reporter: null,
     publicPath: config.output.publicPath
   }))
+
+  app.use(webpackHotMiddleware(compiler))
 }
 
 app.listen(port)
--- app/components/home/home_component.jsx
+++ app/components/home/home_component.jsx
@@ -4,14 +4,14 @@ import TransitionGroup from 'react-addons-transition-group';
 const Home = () => {
   return (
     <div className='home'>
-      <div className='container '>
+      <div className='container'>
         <div className='left-half'>Artist</div>
-        <div className='short-bio'>@ the Wurks</div>
+        <div className='short-bio'><a href='http://thewurks.com'>@ the Wurks</a></div>
       </div>
       <div className='container pull-right'>
         <div className='right-half'>Engineer</div>
         <div className='pull-right'>
-          <div className='short-bio'>@ Upserve</div>
+          <div className='short-bio'><a href='http://upserve.com'>@ Upserve</a></div>
         </div>
       </div>
       <div className='footer'>
