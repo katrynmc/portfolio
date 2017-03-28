3b9ac161ab80ce6a1227a0d3683d9794c53fa125
Katryn McIntosh <katrynmcintosh@swipely.com>
2017-03-28 00:56:01 -0400

    prod ready

--- app/components/widgets/nav_bar_component.jsx
+++ app/components/widgets/nav_bar_component.jsx
@@ -5,8 +5,8 @@ const NavBar = () => {
   return (
     <div className='nav'>
       <div className='name'><Link to="/">KATRYN MCINTOSH</Link></div>
-      <div className='section art'><Link to="/art">ART</Link></div>
-      <div className='section'><Link to="/engineering">ENGINEERING</Link></div>
+      <div className='section art'><Link to="">ART</Link></div>
+      <div className='section'><Link to="">ENGINEERING</Link></div>
     </div>
   );
 };
