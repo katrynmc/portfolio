08248299902664d114ee52230da3216c0d673635
Katryn McIntosh <katrynmcintosh@swipely.com>
2017-03-28 00:40:13 -0400

    Redo and strip down desing

--- app/components/home/home_component.jsx
+++ app/components/home/home_component.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import TransitionGroup from 'react-addons-transition-group';
 import Repeat from 'react-icons/lib/md/repeat';
-const face = require('../../assets/images/myface.png');
-
+const face = require('../../assets/images/newcolor2.png');
+const eyes = require('../../assets/images/myface.png');
 const Home = () => {
   return (
     <div className='home'>
-      <div className='container-left'>
-        <div className='main-card'>
-          <div className='profession art'>Artist <Repeat /></div>
-          <div className='profession engineer'>Engineer</div>
+
+
+
+
+        <div className='bio'>
+        <div className='profession art'>ARTIST! </div>
+          <div className='profession engineer'>ENGINEER!</div>
+          Katryn is an artist and UX engineer. Initially trained in fine art, she graduated from the Rhode Island School of Design in 2007 with a BFA in painting. Upon discovering a passion for programming she pursued a career in technology. She currently works for Upserve as a UX engineer. She is also a member of the artist collective the Wurks.
         </div>
 
-        <div className='background-triangle left' />
       </div>
-      <div className='container-right'>
-        <div className='bio'>
-          katryn is an artist and ux engineer based in providence, rhode island. initially trained in fine art she graduated from the rhode island school of design in 2007 with a bfa in painting. on discovering a passion for programming she pursued a career in technology. she currently works for Upserve as a ux engineer. she is a member of the artist collective the Wurks.
-        </div>
-        <div className='background-triangle' />
-      </div>
-    </div>
+
   );
 }
 
 export default Home;
 
 
-// <div className='faces'>
-//           <img
-//             className='face one'
-//             src={face}
-//             alt='selfie bomb :: warhol style'/>
-//           <img
-//             className='face two'
-//             src={face}
-//             alt='selfie bomb :: warhol style'/>
-//           <img
-//             className='face three'
-//             src={face}
-//             alt='selfie bomb :: warhol style'/>
-//         </div>
+
--- app/components/widgets/nav_bar_component.jsx
+++ app/components/widgets/nav_bar_component.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router'
 const NavBar = () => {
   return (
     <div className='nav'>
-      <div className='name'><Link to="/">katryn<br />mcIntosh</Link></div>
-      <div className='section art'><Link to="/art">art</Link></div>
-      <div className='section'><Link to="/engineering">engineering</Link></div>
+      <div className='name'><Link to="/">KATRYN MCINTOSH</Link></div>
+      <div className='section art'><Link to="/art">ART</Link></div>
+      <div className='section'><Link to="/engineering">ENGINEERING</Link></div>
     </div>
   );
 };
