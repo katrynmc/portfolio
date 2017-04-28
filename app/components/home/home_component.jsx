e409531645fe048a2df82af4f66523db25c67ecf
Katryn McIntosh <katrynmcintosh@swipely.com>
2017-04-28 01:22:21 -0400

    Add in bunny pic

--- app/components/home/home_component.jsx
+++ app/components/home/home_component.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import TransitionGroup from 'react-addons-transition-group';
 
+const bunnyHelmet = require('../../assets/images/bunny_helmet.png');
+
 const Home = () => {
   return (
     <div className='home'>
       <div className='bio'>
         <div className='profession art'>ARTIST! </div>
         <div className='profession engineer'>ENGINEER!</div>
+        <img
+          className='front-image'
+          src={bunnyHelmet}
+          alt='Digital drawing - helmet with two mounted bunny heads' />
+
         <p>Initially trained in fine art, Katryn graduated from the Rhode Island School of Design in 2007 with a BFA in Painting. Upon discovering a passion for programming she pursued a career in technology. She currently works for <span className='company'>Upserve</span> in Providence, RI and is a member of the artist collective <span className='company'>the Wurks</span>.</p>
       </div>
     </div>
