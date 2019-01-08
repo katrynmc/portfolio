896fe5c40cb4c501836535b2b7a5bf52687b0c9e
Katryn McIntosh <katryn.mcintosh@gmail.com>
2019-01-07 22:56:49 -0500

    Update webpack (prep for ts)

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,53 +1,67 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: [
-    './app/entry'
-  ],
+  entry: ["./app/entry"],
 
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/public/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/public/",
+    filename: "bundle.js"
   },
 
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: [
-          path.resolve(__dirname, 'app')
-        ],
-        options: {
-          presets: ['es2015', 'react']
-        }
+        test: /\.(j|t)sx?$/,
+        include: [path.resolve(__dirname, "app")],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              babelrc: false,
+              presets: [
+                [
+                  "@babel/preset-env",
+                  { targets: { browsers: "last 2 versions" } }
+                ],
+                "@babel/preset-typescript",
+                "@babel/preset-react"
+              ],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "react-hot-loader/babel"
+              ]
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'file-loader?name=img/img-[hash:6].[ext]'
+        loader: "file-loader?name=img/img-[hash:6].[ext]"
       },
       {
         test: /\.styl$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader"
           },
           {
-            loader: 'css-loader'
+            loader: "css-loader"
           },
           {
-            loader: 'autoprefixer-loader'
+            loader: "autoprefixer-loader"
           },
           {
-            loader: 'stylus-loader',
+            loader: "stylus-loader",
             options: {
-              use: [require('nib')()],
-              import: ['~nib/lib/nib/index.styl']
+              use: [require("nib")()],
+              import: ["~nib/lib/nib/index.styl"]
             }
-          },
-        ],
+          }
+        ]
       }
     ]
   },
@@ -60,16 +74,16 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     })
   ],
 
   resolve: {
-    extensions: ['.js', '.jsx', '.styl'],
+    extensions: [".js", ".jsx", ".styl"],
     alias: {
-      Images: path.resolve(__dirname, 'app/assets/images/')
+      Images: path.resolve(__dirname, "app/assets/images/")
     }
   }
 };
