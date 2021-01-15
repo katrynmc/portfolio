4309ea6b4b28a2cf953a9c53efacbc8015526c11
Katryn McIntosh <kmcintosh@salsify.com>
2021-01-14 22:43:56 -0500

    Make home content _okay_ on mobile

--- src/components/section/home/home.tsx
+++ src/components/section/home/home.tsx
@@ -6,13 +6,13 @@ import {
   summary,
   venn,
   circleA,
+  home,
 } from "./home-styles.css";
 
 function Home(): React.ReactElement {
   return (
-    <>
+    <div className={home}>
       <div className={motivation}>
-        <h1 className={focusHeading}>Art & Engineering</h1>
         <div className={venn}>
           <div className={circleA}>&nbsp;</div>
           <div className={circleA}>&nbsp;</div>
@@ -21,38 +21,17 @@ function Home(): React.ReactElement {
           <div className={circleA}>&nbsp;</div>
           <div className={circleA}>&nbsp;</div>
         </div>
-        <div className={venn}>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-        </div>
-        <div className={venn}>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-        </div>
-        <div className={venn}>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-          <div className={circleA}>&nbsp;</div>
-        </div>
-        <p className={summary}>
-          I&apos;m passionate about building inspiring (and maintainable!)
-          interfaces and experiences with great people. I value engagement with
-          inclusive teams and believe that both art and technology have the
-          potential to enrich peoples lives.
-        </p>
       </div>
-    </>
+
+      <h1 className={focusHeading}>Art & Engineering</h1>
+
+      <p className={summary}>
+        I&apos;m passionate about building inspiring (and maintainable!)
+        interfaces and experiences with great people. I value engagement with
+        inclusive teams and believe that both art and technology have the
+        potential to enrich peoples lives.
+      </p>
+    </div>
   );
 }
 
