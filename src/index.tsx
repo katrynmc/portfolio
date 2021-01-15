91b59883a4a49ae6c61cc665e33108c18fa521e2
Katryn McIntosh <kmcintosh@salsify.com>
2021-01-14 19:21:28 -0500

    Fix app height for mobile

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,6 @@ import "../style.css";
 import App from "./components/app/app";
 
 const appDiv = document.createElement("div");
+appDiv.style.height = "100%";
 
 render(<App />, document.body.appendChild(appDiv));
